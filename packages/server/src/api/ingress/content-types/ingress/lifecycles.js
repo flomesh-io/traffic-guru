4e891a3babf08aee8f74bc88837611b27ae96762
zhangcheng870518 <zhangcheng@flomesh.cn>
2023-04-12 14:45:31 +0800

    fix menu issue

--- packages/server/src/api/ingress/content-types/ingress/lifecycles.js
+++ packages/server/src/api/ingress/content-types/ingress/lifecycles.js
@@ -1,7 +1,4 @@
 "use strict";
 
 module.exports = {
-  beforeCreate: (event) => strapi.service('api::resource.resource').make(event, 'ingress'),
-  afterCreate: (event) => strapi.service('api::resource.resource').deploy(event),
-  afterUpdate: (event) => strapi.service('api::resource.resource').deploy(event),
 };  
